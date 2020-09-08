eb7eef987ece0752d7f4164a2b927eb9daba273d
Peter Wielander <mittgfu@gmail.com>
2020-09-08 21:35:49 +0200

    1.0.2: fix time interval precision

--- newtabscript.js
+++ newtabscript.js
@@ -5,7 +5,7 @@ const age = localStorage.getItem("age") || "1990-01-01"
 let time = (new Date(new Date() - new Date(age))) / 1000 / 3600 / 24 / 365;
 
 setInterval(() => {
-	time += 50 / 3600 / 24 / 365;
-	timer.innerText = time.toFixed(6);
+	time += 1 / (365 * 24 * 3600 * 20)
+	timer.innerText = time.toFixed(8);
 }, 50)
 
