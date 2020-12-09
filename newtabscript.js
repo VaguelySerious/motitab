7a5f73bb1241cc54d83b6dd8aba7d0ea776397c2
Peter Wielander <mittgfu@gmail.com>
2020-12-09 19:33:44 +0100

    Fix inaccurate date

--- newtabscript.js
+++ newtabscript.js
@@ -2,10 +2,18 @@ const timer = document.querySelector('#timer');
 
 const age = localStorage.getItem("age") || "1990-01-01"
 
-let time = (new Date(new Date() - new Date(age))) / 1000 / 3600 / 24 / 365;
+const currentYear = new Date().getYear()
+const birthYear = new Date(age.slice(0, 4) + '-06-06').getYear()
+const birthDateinCurrentYear = new Date('20' + (currentYear - 100) + age.slice(4))
+
+let years = currentYear - birthYear - 1
+let time = 1 - (birthDateinCurrentYear - new Date()) / 1000 / 3600 / 24 / 365
 
 setInterval(() => {
-	time += 1 / (365 * 24 * 3600 * 20)
-	timer.innerText = time.toFixed(8);
+ time += 1 / (365 * 24 * 3600 * 20)
+ if (time >= 1) {
+  years += 1
+  time -= 1
+ }
+ timer.innerText = (years + time).toFixed(8);
 }, 50)
-
